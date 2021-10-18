7bb6e5e328a0e6cd799ebf511dffd0de974f5f7d
GamingSupreme <joshualeite2003@gmail.com>
2021-10-18 12:06:37 +0800

    Added player collision
    player can hit enemies

---
+++ Jasper's Home Invasion (and mass stamp heist)/Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void Start()
+    {
+        //Set the tag of this GameObject to Player
+        gameObject.tag = "Enemy";
+    }
+}
--- Jasper's Home Invasion (and mass stamp heist)/Assets/Scripts/PlayerMovement.cs
+++ Jasper's Home Invasion (and mass stamp heist)/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,12 @@ public class PlayerMovement : MonoBehaviour
     bool Jump = false;
     bool Crouch = false;
 
+    void Start()
+    {
+        //Set the tag of this GameObject to Player
+        gameObject.tag = "Player";
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -29,11 +35,20 @@ public class PlayerMovement : MonoBehaviour
         {
             Crouch = false;
         }
+
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, Crouch, Jump);
         Jump = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            Debug.Log("Damaged");
+        //if (collision.gameObject.tag == "Health")
+        //    Debug.Log("Healed");
     }
 }
