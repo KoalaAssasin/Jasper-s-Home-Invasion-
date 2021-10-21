0a9188a8797513fdd8c55ded24e584f68c1a709c
GamingSupreme <joshualeite2003@gmail.com>
2021-10-21 16:07:02 +0800

    HitBox and Enemies
    Fixed the hit box a bit, still sometimes takes damage when in running animation and hitting enemy
    
    Also added enemies respawning in random positions after a short delay of dying

--- Jasper's Home Invasion (and mass stamp heist)/Assets/Scripts/Enemy.cs
+++ Jasper's Home Invasion (and mass stamp heist)/Assets/Scripts/Enemy.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Object enemyRef;
+
+    Vector3 startPos;
 
     void Start()
     {
+        startPos = transform.position;
+        enemyRef = Resources.Load("Bunni");
         //Set the tag of this GameObject to Player
         gameObject.tag = "Enemy";
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Respawn()
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            
-        }
+        GameObject enemyClone = (GameObject)Instantiate(enemyRef);
+        enemyClone.transform.position = new Vector3(UnityEngine.Random.Range(startPos.x - 1, startPos.x + 1), startPos.y, startPos.z);
+
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,7 +31,11 @@ public class Enemy : MonoBehaviour
             if (collision.gameObject.GetComponent<Rigidbody2D> ().velocity.y <= 0f)
             {
                 collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(10.0f, 10.0f);
-                Destroy(gameObject);
+                //Destroy(gameObject);
+                gameObject.SetActive(false);
+
+                Invoke("Respawn", 5);
+
             }
         }
     }
